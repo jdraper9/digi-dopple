df48f3995986fd25ec61280992d0e81ca770cc9e
jdraper9 <jamesdraper24@gmail.com>
2020-03-13 15:47:25 -0700

    beautiful

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,29 +2,46 @@ import React, { Component } from 'react';
 import { Image, Grid, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { logout } from '../helpers/auth';
+import FileUploader from 'react-firebase-file-uploader';
+import firebase from 'firebase';
+
 const appTokenKey = 'appToken';
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    const allPhotos = [
-      {
-        id: 'randomstringimadeup43454356546',
-        url: 'http://www.fillmurray.com/200/300'
-      },
-      {
-        id: 'randomstringimadeup43523526534565',
-        url: 'http://www.fillmurray.com/200/300'
-      },
-      {
-        id: 'randomstringimadeup433245234534',
-        url: 'http://www.fillmurray.com/200/300'
-      }
-    ];
+
     this.state = {
-      allPhotos
+      allPhotos: []
     };
+
     this.handleLogout = this.handleLogout.bind(this);
+    this.getInitial = this.getInitial.bind(this);
   }
+
+  componentDidMount() {
+    this.getInitial();
+  }
+
+  getInitial() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        firebase
+          .firestore()
+          .collection('photos')
+          .onSnapshot(snapshot => {
+            let allPhotos = [];
+            snapshot.forEach(doc => {
+              var newItem = doc.data();
+              newItem.id = doc.id;
+              allPhotos.push(newItem);
+            });
+
+            this.setState({ allPhotos });
+          });
+      }
+    });
+  }
+
   handleLogout() {
     logout().then(() => {
       localStorage.removeItem(appTokenKey);
@@ -32,6 +49,45 @@ export default class Home extends Component {
       console.log('user signed out from firebase');
     });
   }
+
+  async handleUploadSuccess(filename) {
+    try {
+      let { bucket, fullPath } = await firebase
+        .storage()
+        .ref('images')
+        .child(filename)
+        .getMetadata();
+      console.log('bucket', bucket);
+      console.log('fullPath', fullPath);
+      let downloadURL = await firebase
+        .storage()
+        .ref('images')
+        .child(filename)
+        .getDownloadURL();
+      console.log('downloadURL', downloadURL);
+
+      let { uid, email, displayName } = await firebase.auth().currentUser;
+
+      let newPhoto = {
+        url: downloadURL,
+        userName: displayName,
+        userId: uid,
+        email,
+        bucket,
+        fullPath
+      };
+      console.log('newPhoto', newPhoto);
+
+      let photoAdded = await firebase
+        .firestore()
+        .collection('photos')
+        .add(newPhoto);
+      console.log('photoAdded', photoAdded);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   render() {
     // our doppelganger images
     const allImages = this.state.allPhotos.map(photo => {
@@ -44,9 +100,11 @@ export default class Home extends Component {
         </div>
       );
     });
+
     return (
       <div>
         {allImages}
+
         <Grid className="bottom-nav">
           <Row className="show-grid">
             <Col xs={4} className="col-bottom">
@@ -55,7 +113,18 @@ export default class Home extends Component {
               </Link>
             </Col>
             <Col xs={4} className="col-bottom">
-              <i className="bottom-icon material-icons">camera_alt</i>
+              <label>
+                <i className="bottom-icon material-icons">camera_alt</i>
+                <FileUploader
+                  hidden
+                  accept="image/*"
+                  storageRef={firebase.storage().ref('images')}
+                  onUploadStart={this.handleUploadStart}
+                  onUploadError={this.handleUploadError}
+                  onUploadSuccess={this.handleUploadSuccess}
+                  onProgress={this.handleProgress}
+                />
+              </label>
             </Col>
             <Col onClick={this.handleLogout} xs={4} className="col-bottom">
               <i className="bottom-icon material-icons">assignment_return</i>
